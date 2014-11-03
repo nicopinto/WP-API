80bec7a0e471ec466cb6e0e7a84885e9faa6dd39
Rachel Baker <rachel@rachelbaker.me>
2014-11-03 09:02:11 -0600

    Change to single quotes in Controller WP_Error messages.

--- lib/class-wp-json-controller.php
+++ lib/class-wp-json-controller.php
@@ -10,28 +10,28 @@ abstract class WP_JSON_Controller {
 	 * @return array|WP_Error
 	 */
 	public function get_items( WP_JSON_Request $request ) {
-		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
+		return new WP_Error( 'invalid-method', __( 'Method not implemented. Must be over-ridden in subclass.' ), array( 'status' => 405 ) );
 	}
 
 	/**
 	 * Get one item from the collection
 	 */
 	public function get_item( WP_JSON_Request $request ) {
-		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
+		return new WP_Error( 'invalid-method', __( 'Method not implemented. Must be over-ridden in subclass.' ), array( 'status' => 405 ) );
 	}
 
 	/**
 	 * Update one item from the collection
 	 */
 	public function update_item( WP_JSON_Request $request ) {
-		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
+		return new WP_Error( 'invalid-method', __( 'Method not implemented. Must be over-ridden in subclass.' ), array( 'status' => 405 ) );
 	}
 
 	/**
 	 * Delete one item from the collection
 	 */
 	public function delete_item( WP_JSON_Request $request ) {
-		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
+		return new WP_Error( 'invalid-method', __( 'Method not implemented. Must be over-ridden in subclass.' ), array( 'status' => 405 ) );
 	}
 
 	/**
@@ -41,7 +41,7 @@ abstract class WP_JSON_Controller {
 	 * @return object
 	 */
 	public function prepare_item_for_response( $item ) {
-		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
+		return new WP_Error( 'invalid-method', __( 'Method not implemented. Must be over-ridden in subclass.' ), array( 'status' => 405 ) );
 	}
 
 }
